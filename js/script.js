0df067413b4f0be08d86044e1b2b2a1f9e9aa23c
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-09 11:51:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,13 +11,11 @@ $(function($){
 	    	nome: $("#nome").val(),
 	    	email: $("#email").val(),
 	    	telefone: $("#telefone").val(),
-            	mensagem: $("#mensagem").val()
+           	mensagem: $("#mensagem").val(),
 	    },
-	    dataType: "json",
-		beforeSend: function() {
-			$("#form").append('<div class="alert alert--loading">Sending message…</div>');
-		},
-		}).done(function(){
+	    dataType: "json"
+		})
+        .done(function(){
 			$("#send").html("<div class='alert alert-success'>");
             $("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
@@ -26,11 +24,12 @@ $(function($){
             
             $('#form').trigger("reset");
             
-		}).fail(function(){
+		})
+        .fail(function(){
 			$("#send").html("<div class='alert alert-danger'>");
             $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!!!"));
+            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
             $("#send > .alert-danger").append('</div>');
             
             $('#form').trigger("reset");
