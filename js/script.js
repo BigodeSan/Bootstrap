0c17eef1737c5853a139db1ff72dfeea536385bc
BigodeSan <40863800+BigodeSan@users.noreply.github.com>
2018-08-09 13:17:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,27 @@
-$(function($){
-
-	$("form").submit(function(event) {
-
-		event.preventDefault();
-
-	$.ajax({
-	    url:'https://formspree.io/sensualmdintima@gmail.com', 
-	    method: "POST",
-	    data: {
-	    	nome: $("#nome").val(),
-	    	email: $("#email").val(),
-	    	telefone: $("#telefone").val(),
-           	mensagem: $("#mensagem").val(),
-	    },
-	    dataType: "json"
-		})
-        .done(function(){
-			$("#send").html("<div class='alert alert-success'>");
-            $("#send > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-            $("#send > .alert-success").append($("<strong>").text("Email enviado com sucesso! retornaremos em breve."));
-            $("#send > .alert-success").append('</div>');
-            
-            $('#form').trigger("reset");
-            
-		})
-        .fail(function(){
-			$("#send").html("<div class='alert alert-danger'>");
-            $("#send > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-            $("#send > .alert-danger").append($("<strong>").text("Desculpe, não foi possivel enviar seu email. Tente mais tarde!"));
-            $("#send > .alert-danger").append('</div>');
-            
-            $('#form').trigger("reset");
-            
-		});
-	});
-
-}) ;
+$(document).ready(function() {
+  $('#form').submit(function(e) {
+      var name = $('#nome')
+      var email = $('#email')
+      var message = $('#mensagem')
+    
+      if(nome.val() == "" || email.val() == "" || mensagem.val() == "") {
+        $('.submit-fail').fadeToggle(400);
+        return false;
+      }
+      else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/sensualmdintima@gmail.com',
+          data: $('#form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        $('.submit-success').fadeToggle(400);
+      }
+    });
+  
+  $('.submit-fail, .submit-success').click(function() {
+    $(this).hide();
+  })
+});
